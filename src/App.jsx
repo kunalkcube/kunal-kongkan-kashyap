2ca24b140068a482e78b02447a8807fea3ffe55c
Kunal Kongkan Kashyap <kunalkashyap3010@gmail.com>
2024-05-01 15:51:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Kunal Kongkan Kashyap</h1>
+      <p>Building my new portfolio website 🚀</p>
+    </>
+  )
+}
+
+export default App
