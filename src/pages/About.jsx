cfc385958928249c2641553bcfe164b388b9be99
Kunal Kongkan Kashyap <kunalkashyap3010@gmail.com>
2024-05-13 21:21:42 +0530

    Add responsive styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,44 +22,44 @@ const About = () => {
                             <div className='animate-ping w-3 h-3 rounded-full bg-primary'></div>
                             <div className='absolute top-0 right-0 w-3 h-3 rounded-full bg-primary'></div>
                         </div>
-                        <h2 className='text-xl mb-1 font-teko font-bold'>Gauhati University</h2>
-                        <h3 className='text-lg mb-1 font-teko font-semibold'>Computer Science</h3>
-                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1'>Integrated MSc</span>
+                        <h2 className='text-xl mb-1 font-teko font-bold lg:text-2xl'>Gauhati University</h2>
+                        <h3 className='text-lg mb-1 font-teko font-semibold lg:text-xl'>Computer Science</h3>
+                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1 lg:text-lg'>Integrated MSc</span>
                     </div>
                     <div class="relative p-2 mt-5 border border-label">
                         <div className='absolute top-2 right-2 w-3 h-3 rounded-full bg-primary'></div>
-                        <h2 className='text-xl mb-1 font-teko font-bold'>Gateway Senior Secondary School</h2>
-                        <h3 className='text-lg mb-1 font-teko font-semibold'>Science</h3>
-                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1'>Class 12</span>
+                        <h2 className='text-xl mb-1 font-teko font-bold lg:text-2xl'>Gateway Senior Secondary School</h2>
+                        <h3 className='text-lg mb-1 font-teko font-semibold lg:text-xl'>Science</h3>
+                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1 lg:text-lg'>Class 12</span>
                     </div>
                     <div class="relative p-2 mt-5 border border-label">
                         <div className='absolute top-2 right-2 w-3 h-3 rounded-full bg-primary'></div>
-                        <h2 className='text-xl mb-1 font-teko font-bold'>Adarsha Jatiya Vidyalaya, Bihdia</h2>
-                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1'>Class 10</span>
+                        <h2 className='text-xl mb-1 font-teko font-bold lg:text-2xl'>Adarsha Jatiya Vidyalaya, Bihdia</h2>
+                        <span className='text-base font-teko font-semibold bg-slate-300 px-2 py-1 lg:text-lg'>Class 10</span>
                     </div>
                 </div>
             </section>
             <section className='py-7 px-4 bg-secondary max-w-6xl mx-auto'>
                 <div class="flex flex-col">
-                    <h2 className='text-xl mb-2 font-teko font-bold'>Main Skills</h2>
+                    <h2 className='text-xl mb-2 font-teko font-bold lg:text-2xl'>Main Skills</h2>
                     <div class="flex flex-wrap">
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>HTML</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>CSS</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>JavaScript</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>PHP</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>SQl</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>Tailwind CSS</span>
-                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg font-semibold font-teko'>BootStrap</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>HTML</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>CSS</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>JavaScript</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>PHP</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>SQl</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>Tailwind CSS</span>
+                        <span className='inline-block py-2 px-3 m-1 bg-secondary border border-label text-lg lg:text-xl font-semibold font-teko'>BootStrap</span>
                     </div>
                 </div>
             </section>
             <section className='py-7 px-4 bg-secondary max-w-6xl mx-auto'>
                 <div class="flex flex-col">
-                    <h2 className='text-xl mb-2 font-teko font-bold'>Languages</h2>
+                    <h2 className='text-xl mb-2 font-teko font-bold lg:text-2xl'>Languages</h2>
                     <div class="flex flex-col">
                         <ul className='list-disc'>
-                            <li className='text-lg font-teko mb-1 text-primary relative ml-6'><span className='font-semibold'>English</span> (Intermediate)</li>
-                            <li className='text-lg font-teko mb-1 text-primary relative ml-6'><span className='font-semibold'>Assamese</span> (Native)</li>
+                            <li className='text-lg lg:text-xl font-teko mb-1 text-primary relative ml-6'><span className='font-semibold'>English</span> (Intermediate)</li>
+                            <li className='text-lg lg:text-xl font-teko mb-1 text-primary relative ml-6'><span className='font-semibold'>Assamese</span> (Native)</li>
                         </ul>
                     </div>
                 </div>
